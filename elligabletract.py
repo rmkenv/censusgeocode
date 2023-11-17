5dca1fc3d8dccd4a99388947757fad0bb64da7ca
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2023-11-17 15:49:12 -0500

    Update elligabletract.py

--- elligabletract.py
+++ elligabletract.py
@@ -1,60 +1,40 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd 
 import requests
 
-# Function to get census tract using Census Geocoder API
+# Functions to call API and extract GEOID 
+
 def get_census_tract(street, city, state):
-
-  url = (f"https://geocoding.geo.census.gov/geocoder/geographies/address"
-         f"?street={requests.utils.quote(street)}"
-         f"&city={requests.utils.quote(city)}" 
-         f"&state={requests.utils.quote(state)}"
-         f"&benchmark=Public_AR_Census2020"
-         f"&vintage=Census2020_Census2020"
-         f"&layers=10"
-         f"&format=json")
-
-  response = requests.get(url)
+  # API request
   
-  if response.status_code == 200:
-    return response.json()
-  else:
-    return "Error in API call"
-
-# Extract GEOID and BLOCK from the response
 def extract_geoid_block(data):
+  # Extract GEOID and BLOCK
 
-  try:
-    census_block = data['result']['addressMatches'][0]['geographies']['Census Blocks'][0]
-    geoid = census_block['GEOID']
-    block = census_block['BLOCK']
-    return geoid, block
-  
-  except (IndexError, KeyError):
-    return None, None
-
-# Read in CSV 
 df = pd.read_csv('https://raw.githubusercontent.com/rmkenv/censusgeocode/main/MD_HB550_ECT.csv')
 
-# Streamlit app
 def main():
 
   st.title("Census Tract Finder")
+  
+  street = st.text_input("Street")
+  city = st.text_input("City")
+  state = st.text_input("State")
 
-  # Get user inputs
-  street = st.text_input("Street", "4600 Silver Hill Rd")
-  city = st.text_input("City", "Washington") 
-  state = st.text_input("State", "DC")
-
-  if st.button("Find Census Tract"):
+  if st.button("Find"):
 
     data = get_census_tract(street, city, state)
-    geoid, _ = extract_geoid_block(data)
+    
+    geoid, block = extract_geoid_block(data)
+    zip_code = data['result']['addressMatches'][0]['addressComponents']['zip']
+
+    st.write(f"GEOID: {geoid}")
+    st.write(f"BLOCK: {block}")
+    st.write(f"ZIP Code: {zip_code}")
 
     if geoid in df['GEOID'].values:
-      st.write("Good!") 
+      st.write("This is an eligible location based on MD HB 550")
     else:
-      st.write("Bad")
+      st.write("This is NOT an eligible location based on MD HB 550")
       
 if __name__ == "__main__":
   main()
