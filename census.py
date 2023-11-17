f684fbf95728bc204e61aab5c45e0c848b79d2f2
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2023-11-17 08:56:36 -0500

    Update census.py

--- census.py
+++ census.py
@@ -8,7 +8,7 @@ def get_census_tract(street, city, state):
            f"?street={requests.utils.quote(street)}"
            f"&city={requests.utils.quote(city)}"
            f"&state={requests.utils.quote(state)}"
-           f"&benchmark=Public_AR_Current"
+           f"&benchmark=Public_AR_Census2020"
            f"&vintage=Census2020_Census2020"
            f"&layers=10"
            f"&format=json")
@@ -17,12 +17,7 @@ def get_census_tract(street, city, state):
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        try:
-            # Extracting the geography information
-            geographies = data['result']['geographies']['Census Blocks']
-            return geographies
-        except (IndexError, KeyError):
-            return "No match found or invalid response format"
+        return data
     else:
         return "Error in API call"
 
@@ -37,7 +32,7 @@ def main():
 
     if st.button("Find Census Tract"):
         result = get_census_tract(street, city, state)
-        if isinstance(result, list):
+        if isinstance(result, dict):
             st.json(result)
         else:
             st.write(result)
