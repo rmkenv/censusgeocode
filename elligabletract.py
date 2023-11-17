c1f976625a5ac791a3c51226fbbd50c8e3f280fa
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2023-11-17 15:50:15 -0500

    Update elligabletract.py

--- elligabletract.py
+++ elligabletract.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 import requests
 
-# Functions to call API and extract GEOID 
-
+# Function to get census tract  
 def get_census_tract(street, city, state):
   # API request
   
+# Extract GEOID and BLOCK
 def extract_geoid_block(data):
   # Extract GEOID and BLOCK
 
@@ -15,7 +15,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/rmkenv/censusgeocode/main/MD
 def main():
 
   st.title("Census Tract Finder")
-  
+
   street = st.text_input("Street")
   city = st.text_input("City")
   state = st.text_input("State")
@@ -23,7 +23,7 @@ def main():
   if st.button("Find"):
 
     data = get_census_tract(street, city, state)
-    
+
     geoid, block = extract_geoid_block(data)
     zip_code = data['result']['addressMatches'][0]['addressComponents']['zip']
 
@@ -35,6 +35,6 @@ def main():
       st.write("This is an eligible location based on MD HB 550")
     else:
       st.write("This is NOT an eligible location based on MD HB 550")
-      
+
 if __name__ == "__main__":
   main()
