138b535e163da050a87e951bfadafd79527cc134
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2023-11-17 16:09:34 -0500

    Update elligabletract.py

--- elligabletract.py
+++ elligabletract.py
@@ -1,37 +1,17 @@
-import streamlit as st
-import pandas as pd
-import requests
-
-# Function to get census tract using Census Geocoder API
-def get_census_tract(street, city, state):
-    url = (f"https://geocoding.geo.census.gov/geocoder/geographies/address"
-           f"?street={requests.utils.quote(street)}"
-           f"&city={requests.utils.quote(city)}" 
-           f"&state={requests.utils.quote(state)}"
-           f"&benchmark=Public_AR_Census2020"
-           f"&vintage=Census2020_Census2020"
-           f"&layers=10"
-           f"&format=json")
-
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return "Error in API call"
-
-# Extract GEOID and BLOCK from the response
-def extract_geoid_block(data):
+# Extract GEOID, BLOCK, ZIP, and coordinates from the response
+def extract_details(data):
     try:
-        census_block = data['result']['addressMatches'][0]['geographies']['Census Blocks'][0]
+        address_match = data['result']['addressMatches'][0]
+        census_block = address_match['geographies']['Census Blocks'][0]
         geoid = census_block['GEOID']
         block = census_block['BLOCK']
-        return geoid, block
+        zip_code = address_match['addressComponents']['zip']
+        coordinates = address_match['coordinates']
+        x = coordinates['x']
+        y = coordinates['y']
+        return geoid, block, zip_code, x, y
     except (IndexError, KeyError):
-        return None, None
-
-# Read in CSV 
-df = pd.read_csv('https://raw.githubusercontent.com/rmkenv/censusgeocode/main/MD_HB550_ECT.csv')
+        return None, None, None, None, None
 
 # Streamlit app
 def main():
@@ -44,18 +24,20 @@ def main():
 
     if st.button("Find Census Tract"):
         data = get_census_tract(street, city, state)
-        geoid, block = extract_geoid_block(data)
+        geoid, block, zip_code, x, y = extract_details(data)
 
-        # Display GEOID and BLOCK if they were found
+        # Display GEOID, BLOCK, ZIP, and coordinates if they were found
         if geoid and block:
             st.write(f"GEOID: {geoid}")
             st.write(f"BLOCK: {block}")
+            st.write(f"ZIP Code: {zip_code}")
+            st.write(f"Coordinates: ({y}, {x})")  # Display coordinates (Latitude, Longitude)
 
         # Check eligibility based on GEOID
         if geoid in df['GEOID'].values:
             st.write("This is an eligible location based on MD HB 550") 
         else:
             st.write("This is NOT an eligible location based on MD HB 550")
-            
+
 if __name__ == "__main__":
     main()
