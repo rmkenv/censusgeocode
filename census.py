0f4414de4cbcf528336edb77a9e908ee478d7bf2
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2023-11-17 08:54:20 -0500

    Update census.py

--- census.py
+++ census.py
@@ -2,37 +2,42 @@ import streamlit as st
 import requests
 
 # Function to get census tract using Census Geocoder API
-def get_census_tract(address):
-    # Format the address for URL encoding
-    formatted_address = requests.utils.quote(address)
-    
+def get_census_tract(street, city, state):
     # Construct the API request URL
-    url = (f"https://geocoding.geo.census.gov/geocoder/locations/onelineaddress"
-           f"?address={formatted_address}&benchmark=Public_AR_Current&format=json")
+    url = (f"https://geocoding.geo.census.gov/geocoder/geographies/address"
+           f"?street={requests.utils.quote(street)}"
+           f"&city={requests.utils.quote(city)}"
+           f"&state={requests.utils.quote(state)}"
+           f"&benchmark=Public_AR_Current"
+           f"&vintage=Census2020_Census2020"
+           f"&layers=10"
+           f"&format=json")
 
     # Make the API request
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         try:
-            # Extracting the address match
-            result = data['result']['addressMatches'][0]
-            return result
-        except IndexError:
-            return "No match found"
+            # Extracting the geography information
+            geographies = data['result']['geographies']['Census Blocks']
+            return geographies
+        except (IndexError, KeyError):
+            return "No match found or invalid response format"
     else:
         return "Error in API call"
 
 # Streamlit app layout
 def main():
     st.title("Census Tract Finder")
-    
-    # Address input
-    address = st.text_input("Enter the address:", "4600 Silver Hill Rd, Washington, DC 20233")
-    
+
+    # Address inputs
+    street = st.text_input("Street", "4600 Silver Hill Rd")
+    city = st.text_input("City", "Washington")
+    state = st.text_input("State", "DC")
+
     if st.button("Find Census Tract"):
-        result = get_census_tract(address)
-        if isinstance(result, dict):
+        result = get_census_tract(street, city, state)
+        if isinstance(result, list):
             st.json(result)
         else:
             st.write(result)
