0133fdc14f3a3af7d7ade7cb981ea9124bf7f249
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2023-11-22 12:42:47 -0500

    Update elligabletract.py
    added ability to display COUNTY from JSON API request

--- elligabletract.py
+++ elligabletract.py
@@ -18,20 +18,21 @@ def get_census_tract(street, city, state):
     else:
         return "Error in API call"
 
-# Extract GEOID, BLOCK, ZIP, and coordinates from the response
+# Extract GEOID, BLOCK, ZIP, county, and coordinates from the response
 def extract_details(data):
     try:
         address_match = data['result']['addressMatches'][0]
         census_block = address_match['geographies']['Census Blocks'][0]
         geoid = census_block['GEOID']
         block = census_block['BLOCK']
+        county = census_block['COUNTY']
         zip_code = address_match['addressComponents']['zip']
         coordinates = address_match['coordinates']
         x = coordinates['x']
         y = coordinates['y']
-        return geoid, block, zip_code, x, y
+        return geoid, block, county, zip_code, x, y
     except (IndexError, KeyError):
-        return None, None, None, None, None
+        return None, None, None, None, None, None
 
 # Read in CSV 
 df = pd.read_csv('https://raw.githubusercontent.com/rmkenv/censusgeocode/main/MD_HB550_ECT.csv')
@@ -47,20 +48,21 @@ def main():
 
     if st.button("Find Census Tract"):
         data = get_census_tract(street, city, state)
-        geoid, block, zip_code, x, y = extract_details(data)
+        geoid, block, county, zip_code, x, y = extract_details(data)
 
-        # Display GEOID, BLOCK, ZIP, and coordinates if they were found
+        # Display GEOID, BLOCK, COUNTY, ZIP, and coordinates if they were found
         if geoid and block:
             st.write(f"GEOID: {geoid}")
             st.write(f"BLOCK: {block}")
+            st.write(f"COUNTY: {county}")
             st.write(f"ZIP Code: {zip_code}")
-            st.write(f"Coordinates: (Latitude: {y}, Longitude: {x})")  # Display coordinates
+            st.write(f"Coordinates: (Latitude: {y}, Longitude: {x})")
 
         # Check eligibility based on GEOID
         if geoid and geoid in df['GEOID'].values:
-            st.write("This is an eligible location based on MD HB 550") 
+            st.write("This location is eligible based on MD HB 550") 
         else:
-            st.write("This is NOT an eligible location based on MD HB 550")
+            st.write("This location is NOT eligible based on MD HB 550")
 
 if __name__ == "__main__":
     main()
