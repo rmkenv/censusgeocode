216d8b266203b22606a7f3fe032291644050f82c
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2023-12-01 14:04:16 -0500

    Update elligabletract.py

--- elligabletract.py
+++ elligabletract.py
@@ -94,7 +94,7 @@ def main():
                 st.markdown("---")  # Thick horizontal line
                 with st.expander("Schools Information"):
                 # Adding normal text note under header for schools 
-                st.text("This section shows information about schools associated with your address search. Note that some listed schools may not be physically located in an eligible census tract. However, they may enroll students who reside in qualifying tracts. Please contact MEA if you need help determining your eligibility based on school attendance boundaries. Feel free to reach out with any other questions as well.")
+                    st.text("This section shows information about schools associated with your address search. Note that some listed schools may not be physically located in an eligible census tract. However, they may enroll students who reside in qualifying tracts. Please contact MEA if you need help determining your eligibility based on school attendance boundaries. Feel free to reach out with any other questions as well.")
                 schools_matched = [feature['properties'] for feature in school_data['features']
                                    if feature['properties']['GEOID20'] == geoid]
                 if schools_matched:
