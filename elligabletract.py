8e463b36025a3b384862a4f74f6e39eeeadbee6c
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2023-12-01 14:23:00 -0500

    Update elligabletract.py

--- elligabletract.py
+++ elligabletract.py
@@ -81,8 +81,8 @@ def main():
                 st.markdown("---")  # Thick horizontal line
                 st.subheader("HB550 Information")  # Subheader for HB550
                 # Add a link to a PDF
-                    pdf_url = "https://mgaleg.maryland.gov/2023RS/chapters_noln/Ch_98_hb0550T.pdf"
-                    st.markdown(f"[View HB550 PDF Document]({pdf_url})", unsafe_allow_html=True)
+                pdf_url = "https://mgaleg.maryland.gov/2023RS/chapters_noln/Ch_98_hb0550T.pdf"
+                st.markdown(f"[View HB550 PDF Document]({pdf_url})", unsafe_allow_html=True)
                 hb550_match = next((item for item in hb550_data if item['GEOID'] == geoid), None)
                 
                 if hb550_match:
